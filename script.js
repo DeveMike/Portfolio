3cb399df83722be68625d159a24f3d781b2634f1
Mihail Rulinskiy <roulinski@hotmail.com>
2023-05-12 00:31:49 +0300

    css

---
+++ script.js
@@ -0,0 +1,35 @@
+
+const carousel = document.querySelector('.carousel');
+const prevBtn = document.querySelector('.prev');
+const nextBtn = document.querySelector('.next');
+
+let scrollAmount = 0;
+let projectWidth = carousel.querySelector('.project').clientWidth;
+
+nextBtn.addEventListener('click', function() {
+  carousel.scrollBy({
+    left: projectWidth,
+    top: 0,
+    behavior: 'smooth'
+  });
+  scrollAmount += projectWidth;
+  checkControls();
+});
+
+prevBtn.addEventListener('click', function() {
+  carousel.scrollBy({
+    left: -projectWidth,
+    top: 0,
+    behavior: 'smooth'
+  });
+  scrollAmount -= projectWidth;
+  checkControls();
+});
+
+    
+
+
+
+
+
+    
